a8f3bdb8f0a89ac09fb0878e097890523a99dcd0
Bhuvan_Parashar <mishrasakshi076@gmail.com>
2023-04-04 14:52:40 +0530

    data

--- api/Serilizers/EmployeeMasterSerializer.py
+++ api/Serilizers/EmployeeMasterSerializer.py
@@ -1,8 +1,8 @@
-from ..databaseModels.employee_master import employee_master
-from rest_framework import serializers
+#from ..databaseModels.employee_master import employee_master
+#from rest_framework import serializers
 
 
-class employeeMasterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = employee_master
-        fields = '__all__'
+#class employeeMasterSerializer(serializers.ModelSerializer):
+#    class Meta:
+#        model = employee_master
+#        fields = '__all__'
